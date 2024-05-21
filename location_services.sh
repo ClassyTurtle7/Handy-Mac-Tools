89a3c65a3acefeed4806388fa3f38227d9cb03e3
Jono <149123604+ClassyTurtle7@users.noreply.github.com>
2024-05-21 11:28:32 +1000

    Update location_services.sh

--- location_services.sh
+++ location_services.sh
@@ -5,7 +5,7 @@
 #            Name:  Jamf Protect Location Services Check Permissions.sh
 #     Description:  Corrects permissions on the plist controlling Location
 #                   Services settings.
-#      Created by:  Jono Loveys
+#      Created by:  Jono (ClassyTurtle7)
 #         Version:  1.0
 
 /usr/bin/sudo /usr/bin/defaults write /Library/Preferences/com.apple.locationmenu.plist ShowSystemServices -bool true
